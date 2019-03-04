7b386bedaa5a7a626d16f99b1f62368cebb730ff
Paweł Dąbała <pawel.dabala@global.com>
2019-03-04 14:32:21 +0100

    startad making setrowdata

--- 001 Project/001 Application/forms/filters.py
+++ 001 Project/001 Application/forms/filters.py
@@ -2,17 +2,45 @@ import sys
 from PySide2.QtWidgets import *
 from PySide2.QtCore import Qt
 from uifilters import Ui_Dialog
+from PySide2.QtGui import QStandardItemModel, QStandardItem
 
 
 class FiltersForm(QDialog):
-    def __init__(self, row_nr, filtername, columns, haders, parent=None):
+    def __init__(self, row_nr, filtername, columns, headersname, parent=None):
         super(FiltersForm, self).__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.setWindowModality(Qt.ApplicationModal)
-        print(filtername)
-        #print(columns)
-        print(haders)
+        self.table = self.ui.tableViewcolumns
+        self.sti = QStandardItemModel()
+        self.table.setModel(self.sti)
+
+        """
+        set controls data
+        """
+        self.sti.setHorizontalHeaderLabels(headersname)
+        self.sti.setColumnCount(len(headersname))
+
+    def setrowdata(self, columns):
+        """
+        add data to rows
+        :param columns:
+        :return:
+        """
+        for row in columns:
+            for col in row:
+                Tutaj kontynuj!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                item = QStandardItem(col)
+                item.setCheckable(True)
+                item.setEditable(False)
+
+
+
+
+
+
+
+
 
 
 if __name__=='__main__':
