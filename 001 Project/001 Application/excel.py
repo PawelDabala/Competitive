85ce3d6c3b8d6b713d2a1e6572489401550f1c52
Paweł Dąbała <pawel.dabala@global.com>
2019-04-09 10:34:26 +0200

    chenge position count column to 24 position

--- 001 Project/001 Application/excel.py
+++ 001 Project/001 Application/excel.py
@@ -44,8 +44,8 @@ class Excel:
         if tech:
             print("rozmiesc kolumny w kolejnosci techenge")
             colout = cls.chenge_poz(colout,
-                                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 9, 14, 11, 17, 10, 15, 16, 18],
-                                    [6, 7, 8, 9, 15, 16, 18, 19, 22, 23, 24, 25]
+                                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 9, 14, 11, 18, 17, 10, 15, 16],
+                                    [6, 7, 8, 9, 15, 16, 18, 19, 22, 23, 25]
                                     )
         else:
             for i in range(4):
